05c2b9fafa070feacd0bcf72330a84a0770f6900
Colin Malcolm <colinmalcolm8@gmail.com>
2021-05-24 21:02:44 -0400

    added Intro section content without styling

---
+++ client/src/components/Intro.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+export default function Intro() {
+  return (
+    <div>
+      <div className="hero">
+        <div className="content">
+          <h1>Hi! I'm Colin Malcolm a Software Engineer</h1>
+
+          <div className="meet"></div>
+          <span>👇</span>
+          <p>Learn more below</p>
+        </div>
+
+        
+        <svg xmlns="http://www.w3.org/2000/svg" width="43" height="73" viewBox="0 0 43 73">
+  <g id="Toggle-1" transform="translate(-248 -716)">
+    <g id="Rectangle_3" data-name="Rectangle 3" transform="translate(248 716)" fill="none" stroke="#fff" stroke-linecap="round" stroke-width="6">
+      <rect width="43" height="73" rx="21.5" stroke="none"/>
+      <rect x="3" y="3" width="37" height="67" rx="18.5" fill="none"/>
+    </g>
+    <ellipse id="Ellipse_1" data-name="Ellipse 1" cx="12.5" cy="13.5" rx="12.5" ry="13.5" transform="translate(257 725)" fill="#fff"/>
+  </g>
+</svg>
+
+
+
+
+      </div>
+    </div>
+  );
+}
