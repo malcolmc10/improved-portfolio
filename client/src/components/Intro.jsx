3f0f187656af9023427e80f15f4930fd30c5f628
Colin Malcolm <colinmalcolm8@gmail.com>
2021-06-02 18:40:07 -0400

    scroll animation complete

--- client/src/components/Intro.jsx
+++ client/src/components/Intro.jsx
@@ -31,7 +31,7 @@ export default function Intro() {
       <rect width="43" height="73" rx="21.5" stroke="none"/>
       <rect x="3" y="3" width="37" height="67" rx="18.5" fill="none"/>
     </g>
-    <ellipse id="Ellipse_1" data-name="Ellipse 1" cx="12.5" cy="13.5" rx="12.5" ry="13.5" transform="translate(257 725)" fill="#fff"/>
+    <ellipse className="circle" id="Ellipse_1" data-name="Ellipse 1" cx="12.5" cy="13.5" rx="12.5" ry="13.5" transform="translate(257 725)" fill="#fff"/>
   </g>
 </svg>
         </div>
