6db37940211d4462d440a2316cd9bf7c9e377902
Colin Malcolm <colinmalcolm8@gmail.com>
2021-06-13 17:28:01 -0400

    fixed finger spacing

--- client/src/components/ContactUs.jsx
+++ client/src/components/ContactUs.jsx
@@ -1,3 +1,56 @@
+import React from 'react'
+
+const ContactUs = () => {
+  return (
+    <div>
+
+
+<div className="container">
+  
+        
+        
+        
+        
+        
+        
+  <div className="screen left">
+          <form className="form">
+            <h1>Contact Me 📧</h1>
+
+
+    </form>
+    
+
+     
+     
+     
+</div>
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  <div class="screen right">
+    pan
+ 
+  
+</div>
+</div>
+
+      
+    </div>
+  )
+}
+
+export default ContactUs
+
+
 
 
 
